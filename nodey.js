2364862e73beb1de239ab0674021d4ee9d23707b
Harshita <harshita19062004@gmail.com>
2023-10-28 13:10:24 +0530

    Day-2

--- nodey.js
+++ nodey.js
@@ -8,3 +8,8 @@ const _=require('lodash');
 const items=[1,[2,[3,[4]]]];
 const newItems=_.flattenDeep(items);
 console.log(newItems);// [1,2,3,4]
+console.log("hello peeps");
+//We are using the dependencies right there
+// how will this will work on others system whosever clone this repo as our node_modules are in .gitignore file
+// ans to this ques is : user just need to install that npm it will automatically have such dependencies and our package.json file is playing
+// a major role by specifically providing the dependencies has been used in the project
